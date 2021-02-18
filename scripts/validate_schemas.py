4d44065ef7fec74194d60f2d1190afcbbbd09a8d
slebras <slebras@lbl.gov>
2021-02-17 17:00:00 -0800

    fixing up tests

--- scripts/validate_schemas.py
+++ scripts/validate_schemas.py
@@ -3,11 +3,10 @@ import yaml
 from jsonschema import validate
 
 
-if len(sys.argv) != 4:
-    raise RuntimeError(f'Please provide validation file, json schema file, and environment as arguments to validate_schemas.py')
+if len(sys.argv) != 3:
+    raise RuntimeError(f'Please provide validation file and json schema file as arguments to validate_schemas.py')
 merged_file = sys.argv[1]
 json_schema_file = sys.argv[2]
-env = sys.argv[3]
 
 with open(json_schema_file) as f:
     _META_VAL_JSONSCHEMA = yaml.safe_load(f)
@@ -60,8 +59,7 @@ files = [
     "validation_files/SESAR.yml",
     "validation_files/ENIGMA.yml",
     "validation_files/miscellaneous.yml",
-    merged_file,
-    f"{env}_metadata_validation.yml"
+    merged_file
 ]
 
 for file in files:
