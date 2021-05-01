7d0ba297bed21e8c5a4447b2ad20535eb89dd1bf
Shane Canon <scanon@lbl.gov>
2021-05-01 16:21:22 -0700

    Created templates and ordered list (WIP)
    This adds some scripts to convert TSV template descriptions into a YAML
    file.  It also adds a script that will merge the templates to create
    an ordered list for presentation.

---
+++ create_ordered.py
@@ -0,0 +1,30 @@
+import yaml
+from yaml import Dumper, Loader
+import json
+import sys
+
+ordered_cat = dict()
+for f in sys.argv[1:]:
+   templ = yaml.load(open(f), Loader=Loader)
+   ordered = [None] * 200
+   for k in templ:
+       ordered[templ[k]['order']] = k
+   for i in ordered:
+       if not i:
+           continue
+       cat = templ[i]['category']
+       if 'transformations' not in templ[i]:
+           continue
+       kbk = templ[i]['transformations'][0]['parameters'][0]
+       if cat not in ordered_cat:
+           ordered_cat[cat] = list()
+       if kbk not in ordered_cat[cat]:
+           ordered_cat[cat].append(kbk)
+
+cat_order = [ 'description', 'geolocation', 'collection', 'curation', 'measurement', 'well', 'relation_to_parent']
+order = {'ordered': []}
+for cat in cat_order:
+    order['ordered'].append({'name': cat, 'ordered_fields': ordered_cat[cat]})
+
+
+print(yaml.dump(order))
